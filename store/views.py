8121b0907a7e5da6a5402fd9a4e831cb21c89d02
sai02-creator <saichaitanyagaddam1@gmail.com>
2024-11-27 23:34:38 +1100

    Create login_user and logout_user functions

--- store/views.py
+++ store/views.py
@@ -1,4 +1,16 @@
 from django.shortcuts import render
+from .models import Product
 
 def home(request):
-    return render(request, 'home.html', {})
+    products = Product.objects.all()
+    return render(request, 'home.html', {'products':products})
+
+
+def about(request):
+    return render(request, 'about.html', {})
+
+def login_user(request):
+     return render(request, 'login.html', {})
+
+def logout_user(request):
+    pass
